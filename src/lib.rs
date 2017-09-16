c8b9bd169c222827dfc896b9dc6780a08048ac7c
Ceri Storey <cez@necrofish.org.uk>
2017-09-16 09:48:40 +0100

    Buffer consumer rows.

--- src/lib.rs
+++ src/lib.rs
@@ -7,13 +7,15 @@ extern crate r2d2_postgres;
 #[macro_use]
 extern crate error_chain;
 
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap,VecDeque};
 use r2d2::Pool;
 use r2d2_postgres::PostgresConnectionManager;
 
+const LIMIT_BUFFER : i64 = 1024;
+
 static INSERT_ROW_SQL: &'static str = "INSERT INTO logs (body) values($1) RETURNING id";
 static SEND_NOTIFY_SQL: &'static str = "SELECT pg_notify('logs', $1 :: bigint :: text)";
-static FETCH_NEXT_ROW: &'static str = "SELECT id, body FROM logs WHERE id > $1 LIMIT 1";
+static FETCH_NEXT_ROW: &'static str = "SELECT id, body FROM logs WHERE id > $1 LIMIT $2";
 static DISCARD_ENTRIES: &'static str = "DELETE FROM logs WHERE id <= $1";
 
 static UPSERT_CONSUMER_OFFSET: &'static str = "INSERT INTO log_consumer_positions (name, \
@@ -113,6 +115,7 @@ pub struct Consumer {
     pool: Pool<PostgresConnectionManager>,
     name: String,
     last_seen_offset: i64,
+    buf: VecDeque<Entry>,
 }
 
 impl Consumer {
@@ -127,6 +130,7 @@ impl Consumer {
             pool: pool,
             name: name.to_string(),
             last_seen_offset: position,
+            buf: VecDeque::new(),
         })
     }
 
@@ -137,24 +141,34 @@ impl Consumer {
     }
 
     fn poll_item(&mut self, conn: &postgres::Connection) -> Result<Option<Entry>> {
+        if let Some(entry) = self.buf.pop_front() {
+            trace!("returning (from buffer): {:?}", entry);
+            self.last_seen_offset = entry.offset;
+            return Ok(Some(entry))
+        }
+
         let t = try!(conn.transaction());
         let next_row = try!(t.prepare_cached(FETCH_NEXT_ROW));
-        let rows = try!(next_row.query(&[&self.last_seen_offset]));
+        let rows = try!(next_row.query(&[&self.last_seen_offset, &LIMIT_BUFFER]));
         debug!("next rows:{:?}", rows.len());
-        let res = if let Some(r) = rows.iter().next() {
+        for r in rows.iter() {
             let id: i64 = r.get(0);
             let body: Vec<u8> = r.get(1);
-            debug!("id: {}", id);
-            self.last_seen_offset = id;
-            Some(Entry {
+            debug!("buffering id: {}", id);
+            self.buf.push_back(Entry {
                 offset: id,
                 data: body,
             })
-        } else {
-            None
         };
-        trace!("returning: {:?}", res);
-        Ok(res)
+
+        if let Some(res) = self.buf.pop_front() {
+            self.last_seen_offset = res.offset;
+            trace!("returning (from db): {:?}", res);
+            Ok(Some(res))
+        } else  {
+            trace!("nothing yet");
+            Ok(None)
+        }
     }
 
     pub fn wait_next(&mut self) -> Result<Entry> {
