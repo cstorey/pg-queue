820ad61eade589e2864f34af1ec4668d8a0e0d28
Ceri Storey <cez@necrofish.org.uk>
2018-12-09 10:25:44 +0000

    Move schema definition to seperate file.

--- src/lib.rs
+++ src/lib.rs
@@ -26,31 +26,7 @@ static FETCH_CONSUMER_POSITION: &'static str = "SELECT position FROM log_consume
                                                 name = $1";
 static LIST_CONSUMERS: &'static str = "SELECT name, position FROM log_consumer_positions";
 static DISCARD_CONSUMER: &'static str = "DELETE FROM log_consumer_positions WHERE name = $1";
-static CREATE_TABLE_SQL: &'static str = "
-        CREATE TABLE IF NOT EXISTS logs (
-            id BIGSERIAL PRIMARY KEY,
-            written_at TIMESTAMPTZ NOT NULL DEFAULT now() ,
-            body bytea NOT NULL
-        );
-        CREATE TABLE IF NOT EXISTS log_consumer_positions (
-            name TEXT PRIMARY KEY,
-            position BIGINT NOT NULL
-        );
-        CREATE INDEX IF NOT EXISTS logs_timestamp_idx ON logs (written_at);
-
-        DO $$
-            BEGIN
-                IF NOT EXISTS (
-                    SELECT true FROM pg_attribute
-                    WHERE attrelid = 'logs'::regclass
-                    AND attname = 'tx_id'
-                    AND NOT attisdropped
-                ) THEN
-                    ALTER TABLE logs ADD COLUMN tx_id BIGINT DEFAULT txid_current();
-                END IF;
-            END
-        $$
-        ";
+static CREATE_TABLE_SQL: &'static str = include_str!("schema.sql");
 
 static LISTEN: &'static str = "LISTEN logs";
 
---
+++ src/schema.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS logs (
+    id BIGSERIAL PRIMARY KEY,
+    written_at TIMESTAMPTZ NOT NULL DEFAULT now() ,
+    body bytea NOT NULL
+);
+CREATE TABLE IF NOT EXISTS log_consumer_positions (
+    name TEXT PRIMARY KEY,
+    position BIGINT NOT NULL
+);
+CREATE INDEX IF NOT EXISTS logs_timestamp_idx ON logs (written_at);
+
+DO $$
+    BEGIN
+        IF NOT EXISTS (
+            SELECT true FROM pg_attribute
+            WHERE attrelid = 'logs'::regclass
+            AND attname = 'tx_id'
+            AND NOT attisdropped
+        ) THEN
+            ALTER TABLE logs ADD COLUMN tx_id BIGINT DEFAULT txid_current();
+        END IF;
+    END
+$$
