c4fce6a36828af5a320665f8f851293d1d7b2ba4
Ceri Storey <cez@necrofish.org.uk>
2021-04-02 17:56:21 +0100

    For debugging, emit the last produced offset in pg notifications.

--- src/logs/producer.rs
+++ src/logs/producer.rs
@@ -1,3 +1,5 @@
+use std::sync::Mutex;
+
 use tokio_postgres::{self, Client, GenericClient};
 use tracing::trace;
 
@@ -5,11 +7,12 @@ use crate::logs::{current_epoch, Error, Result, Version};
 
 static INSERT_ROW_SQL: &str =
     "INSERT INTO logs (epoch, key, meta, body) values($1, $2, $3, $4) RETURNING epoch, tx_id as tx_position, id as position";
-static SEND_NOTIFY_SQL: &str = "SELECT pg_notify('logs', '')";
+static SEND_NOTIFY_SQL: &str = "SELECT pg_notify('logs', $1)";
 
 pub struct Batch<'a> {
     transaction: tokio_postgres::Transaction<'a>,
     insert: tokio_postgres::Statement,
+    last_produced_version: Mutex<Option<Version>>,
     epoch: i64,
 }
 
@@ -37,10 +40,12 @@ impl<'a> Batch<'a> {
         let t = client.transaction().await?;
         let epoch = current_epoch(&t).await?;
         let insert = t.prepare(INSERT_ROW_SQL).await?;
+        let last_produced_version = Mutex::new(None);
         let batch = Batch {
             transaction: t,
             insert,
             epoch,
+            last_produced_version,
         };
         Ok(batch)
     }
@@ -64,7 +69,7 @@ impl<'a> Batch<'a> {
             .map(|r| Version::from_row(r))
             .next()
             .ok_or(Error::NoRowsFromInsert)?;
-
+        *self.last_produced_version.lock().expect("lock") = Some(id);
         trace!(?id, "Produced");
         Ok(id)
     }
@@ -78,8 +83,17 @@ impl<'a> Batch<'a> {
         // However, because it's _really_ awkward_ to get a reference to both
         // a client and it's transaction, we do this inside the transaction
         // and hope for the best.
-        let Batch { transaction, .. } = self;
-        transaction.execute(SEND_NOTIFY_SQL, &[]).await?;
+        let Batch {
+            transaction,
+            last_produced_version,
+            ..
+        } = self;
+        let last_version = last_produced_version.lock().expect("lock").take();
+        if let Some(version) = last_version {
+            transaction
+                .execute(SEND_NOTIFY_SQL, &[&version.to_string()])
+                .await?;
+        }
         trace!("Sent notify");
         transaction.commit().await?;
         trace!("Committed");
